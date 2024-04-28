9e19f5d9b2ab0ec1df14594fd12227627b3304c3
Jim Evans <jim@vheavy.com>
2024-04-27 18:21:46 -0700

    Better mockup of configuration display.  Adjustments work

--- lvglConfigPanel.ino
+++ lvglConfigPanel.ino
@@ -130,11 +130,10 @@ void panel_setup() {
 }
 #endif
 
+// enable font in ~/Arduino/libraries/lvgl/lv_conf_template.h
+static const lv_font_t *default_font =  &lv_font_montserrat_42;
 
-
-static const lv_font_t *default_font =  &lv_font_montserrat_32;
-
-
+// create first tile for monitoring selections 
 void mon_menu_create(lv_obj_t *parent)
 {
     static lv_coord_t col_dsc[] = {50, LV_GRID_FR(2), LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
@@ -183,13 +182,90 @@ void mon_menu_create(lv_obj_t *parent)
     }
 }
 
+const static int nr_rows = 20;
+struct ParamEntry {
+  lv_obj_t *sel;
+  lv_obj_t *val;
+  const char *label;
+  const char *fmt;
+  float inc;
+  float max;
+  float min;
+  bool wrap;
+  const char **enumlabels;
+} rows[nr_rows];
 
+lv_obj_t *multBut;
+
+void btn_event_1x(lv_event_t *e) {
+  lv_obj_t *o = lv_event_get_target(e);
+  lv_obj_t *l = lv_obj_get_child(o, 0);
+  if (l == NULL)
+    return;
+  Serial.printf("pressed\n");
+  const char *t = lv_label_get_text(l);
+  int v;
+  if (t != NULL && sscanf(t, "%dX", &v) == 1) { 
+    v *= 10;
+    if (v > 100) v = 1;
+    lv_label_set_text_fmt(l, "%dX", v);
+  }
+}
+
+int selected_btn = -1;
+void btn_event_sel(lv_event_t *e) {
+  int idx = (int)lv_event_get_user_data(e);
+  if (idx >= 0 && idx < nr_rows) { 
+    if (selected_btn >= 0 && selected_btn < nr_rows) { 
+      lv_obj_t *b = rows[selected_btn].sel;
+      static lv_style_t style_btn_red2;
+      lv_style_init(&style_btn_red2);
+      lv_style_set_bg_color(&style_btn_red2, lv_color_make(0, 0, 200));
+      lv_style_set_bg_opa(&style_btn_red2, LV_OPA_COVER);
+      lv_obj_add_style(b, &style_btn_red2, 0);
+    }
+    if (selected_btn == idx) { 
+      selected_btn = -1;
+    } else { 
+      selected_btn = idx;
+      lv_obj_t *b = rows[selected_btn].sel;
+      static lv_style_t style_btn_red2;
+      lv_style_init(&style_btn_red2);
+      lv_style_set_bg_color(&style_btn_red2, lv_color_make(200, 0, 0));
+      lv_style_set_bg_opa(&style_btn_red2, LV_OPA_COVER);
+      lv_obj_add_style(b, &style_btn_red2, 0);
+    }
+  }   
+}
+
+void btn_event_inc(lv_event_t *e) {
+  int dir = (int)lv_event_get_user_data(e);
+  if (selected_btn < 0 || selected_btn >= nr_rows) 
+    return;
+
+  lv_obj_t *l = lv_obj_get_child(multBut, 0);
+  if (l == NULL)
+    return;
+  const char *t = lv_label_get_text(l);
+  int mult = 1;
+  if (t != NULL)
+    sscanf(t, "%dX", &mult);
+
+  t = lv_label_get_text(rows[selected_btn].val);
+  float val = 0;
+  if (t != 0) 
+    sscanf(t, "%f", &val);
+  val = val + dir * mult;
+  char buf[32];
+  snprintf(buf, sizeof(buf), "%.2f", val);
+  lv_label_set_text(rows[selected_btn].val, buf);
+}
+
+// create tunable parameter configuration tile 
 void conf_menu_create(lv_obj_t *parent)
 {
     static lv_coord_t col_dsc[] = {50, LV_GRID_FR(2), LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
     static lv_coord_t row_dsc[50];
-
-    int nr_rows = 20;
     for (int r = 0; r < nr_rows; r++) { 
 	     row_dsc[r] = 50;
     }
@@ -207,8 +283,6 @@ void conf_menu_create(lv_obj_t *parent)
     lv_obj_set_size(cont2, LV_PCT(100), LV_PCT(20));
     lv_obj_align_to(cont2, cont, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 0);
 
-  
-
     lv_obj_t * label;
     lv_obj_t * obj;
 
@@ -218,6 +292,17 @@ void conf_menu_create(lv_obj_t *parent)
     lv_obj_set_style_text_font(label, default_font, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_center(label);
     lv_obj_align_to(obj, cont2, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_add_event_cb(obj, btn_event_inc, LV_EVENT_CLICKED, (void *)-1);
+
+
+    obj = lv_btn_create(cont2);
+    label = lv_label_create(obj);
+    lv_label_set_text_fmt(label, "1X");
+    lv_obj_set_style_text_font(label, default_font, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_center(label);
+    lv_obj_align_to(obj, cont2, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_event_cb(obj, btn_event_1x, LV_EVENT_CLICKED, NULL);
+    multBut = obj;
 
     obj = lv_btn_create(cont2);
     label = lv_label_create(obj);
@@ -225,7 +310,7 @@ void conf_menu_create(lv_obj_t *parent)
     lv_obj_set_style_text_font(label, default_font, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_center(label);
     lv_obj_align_to(obj, cont2, LV_ALIGN_RIGHT_MID, 0, 0);
-
+    lv_obj_add_event_cb(obj, btn_event_inc, LV_EVENT_CLICKED, (void *)1);
 
     uint32_t i;
     for(i = 0; i < nr_rows; i++) {
@@ -235,9 +320,8 @@ void conf_menu_create(lv_obj_t *parent)
         obj = lv_btn_create(cont);
         lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, 0, 1,
                              LV_GRID_ALIGN_STRETCH, row, 1);
-        //label = lv_label_create(obj);
-        //lv_label_set_text_fmt(label, "c%d, r%d", 0, row);
-        //lv_obj_center(label);
+        lv_obj_add_event_cb(obj, btn_event_sel, LV_EVENT_CLICKED, (void *)i);
+        rows[i].sel = obj;
 
         obj = lv_label_create(cont);
         lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, 1, 1,
@@ -248,8 +332,9 @@ void conf_menu_create(lv_obj_t *parent)
         obj = lv_label_create(cont);
         lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, 2, 1,
                              LV_GRID_ALIGN_END, row, 1);
-        lv_label_set_text_fmt(obj, "VAL%d", row);
+        lv_label_set_text_fmt(obj, "%d", row);
         lv_obj_set_style_text_font(obj, default_font, LV_PART_MAIN | LV_STATE_DEFAULT);
+        rows[i].val = obj;
     }
 }
 
@@ -265,22 +350,20 @@ void tiles_create() {
     lv_obj_t *t2 = lv_tileview_add_tile(tileview, 1, 0, LV_DIR_HOR | LV_DIR_BOTTOM);
     lv_obj_t *t3 = lv_tileview_add_tile(tileview, 2, 0, LV_DIR_HOR | LV_DIR_BOTTOM);
 
-    mon_menu_create(t1);
-    conf_menu_create(t2);
-    lv_btn_create(t3);
+    //mon_menu_create(t1);
+    conf_menu_create(t1);
+    //lv_btn_create(t3);
 }
 
-void setup()
-{
+void setup() {
     Serial.begin(115200); /* prepare for possible serial debug */
     panel_setup();
     tiles_create();
     Serial.println("Setup done");
 }
 
-void loop()
-{
-    Serial.println("Loop");
+void loop(){
+    //Serial.println("Loop");
     lv_timer_handler();
     delay(2);
 }
