a0cc701d3a78133e50a9ec498920f6f9593bab6f
jim@vheavy.com <jim@vheavy.com>
2025-04-14 16:03:37 -0700

    forgot new version of cli-parser.py

--- bin/cli-parser.py
+++ bin/cli-parser.py
@@ -5,8 +5,12 @@ import re
 
 #arduino-cli -v compile --build-path ./build/esp32/ --clean -b esp32:esp32:esp32 --board-options PartitionScheme=min_spiffs -u -p /dev/ttyUSB0
 
+# TODO: needs to parse out archives too 
+archives = ()
 objs = ()
 cmds = {}
+archiveDeps = {}
+archiveCmds = {}
 
 for line in map(str.rstrip, sys.stdin):
     m = re.search('-o ([^ ]+[.]((bin)|(o)|(elf)))', line)
@@ -25,7 +29,6 @@ for line in map(str.rstrip, sys.stdin):
             if m:
                 sketchcpp = m.group(1)
                 sketch = m.group(2)
-
     m = re.search('--port "(/dev/tty[^"]+)"', line)
     if m:
         port = m.group(1)
@@ -33,6 +36,16 @@ for line in map(str.rstrip, sys.stdin):
     m = re.search('FQBN: esp32:esp32:([^:]+)', line)
     if m:
         board = m.group(1)
+    m = re.search('elf-ar cr (.*[.]a) (.*[.]o)', line)
+    if m:
+        if archiveDeps.get(m.group(1)) == None:
+            archiveDeps[m.group(1)] = ""
+            archiveCmds[m.group(1)] = ""
+        archiveDeps[m.group(1)] = archiveDeps[m.group(1)] + " " + m.group(2)
+        archiveCmds[m.group(1)] = archiveCmds[m.group(1)] + "\t" + line + "\n"
+
+
+  
 
 print('PORT:="' + port +'"')
 
@@ -43,6 +56,8 @@ print("\tcat ./" + sketch + ".ino > $@")
 print(elf, end=": ")
 for o in objs:
     print(o, end = " ")
+for ar in archiveCmds:
+    print(ar, end = " ")
 print("\n\t@echo `basename " + elf + "`\n\t@" + elfCmd)
 
 print(binFile + ": " + elf)
@@ -55,6 +70,10 @@ print("\t" + uploadCmd)
 
 for o in objs:
     print(o + ":\n\t@echo `basename " + o + "`\n\tccache " + re.sub("^([^ ]+) ", r"\1 ${EXTRA_CFLAGS} ", cmds[o]))
+
+for ar in archiveCmds:
+    print(ar + ": " + archiveDeps[ar])
+    print(archiveCmds[ar])
 
 print("clean:")
 for o in objs + (binFile, elf,):
